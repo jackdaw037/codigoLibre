03071f7de37c240d073f41dc9922c240f51f1e42
santiago <santisalserin.sr@gmail.com>
2025-12-03 09:52:58 +0100

    3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -5,36 +6,53 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         double resultado = 0;
+        boolean salir = false;
 
-        System.out.print("Ingresa el primer número: ");
-        double num1 = sc.nextDouble();
+        while (!salir) {
+            try {
+                System.out.print("Ingresa el primer número: ");
+                double num1 = sc.nextDouble();
 
-        System.out.print("Ingresa el operador (+, -, *, /): ");
-        char operador = sc.next().charAt(0);
+                System.out.print("Ingresa el operador (+, -, *, /): ");
+                char operador = sc.next().charAt(0);
 
-        System.out.print("Ingresa el segundo número a operar: ");
-        double num2 = sc.nextDouble();
+                System.out.print("Ingresa el segundo número a operar: ");
+                double num2 = sc.nextDouble();
 
-        if (operador == '+') {
-            resultado = num1 + num2;
-            System.out.println("Resultado: " + resultado);
-        } else if (operador == '-') {
-            resultado = num1 - num2;
-            System.out.println("Resultado: " + resultado);
-        } else if (operador == '*') {
-            resultado = num1 * num2;
-            System.out.println("Resultado: " + resultado);
-        } else if (operador == '/') {
-            if (num2 != 0) {
-                resultado = num1 / num2;
-                System.out.println("Resultado: " + resultado);
-            } else {
-                System.out.println("Error: no se puede dividir entre cero.");
+                if (operador == '+') {
+                    resultado = num1 + num2;
+                    System.out.println("Resultado: " + resultado);
+                } else if (operador == '-') {
+                    resultado = num1 - num2;
+                    System.out.println("Resultado: " + resultado);
+                } else if (operador == '*') {
+                    resultado = num1 * num2;
+                    System.out.println("Resultado: " + resultado);
+                } else if (operador == '/') {
+                    if (num2 == 0) {
+                        System.out.println("Error: División entre 0");
+                    } else {
+                        resultado = num1 / num2;
+                        System.out.println("Resultado: " + resultado);
+                    }
+                } else {
+                    System.out.println("Operador no válido.");
+                }
+
+
+                System.out.print("¿Deseas salir? (s/n): ");
+                char respuesta = sc.next().charAt(0);
+                if (respuesta == 's' || respuesta == 'S') {
+                    salir = true;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Entrada inválida.");
+                sc.nextLine();
             }
-        } else {
-            System.out.println("Operador no válido.");
         }
 
         sc.close();
+        System.out.println("Programa finalizado.");
     }
 }
